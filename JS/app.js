440d70c71928e0b58ca761989a0bce6e24b2bd42
Bassilalkhateeb <basilalkhateeb10@gamil.com>
2021-02-15 22:21:27 +0200

    editing

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,7 @@ function getRandomNumberOfCustomersPerHour(min,max){
 
 let workingHour =['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
-const seattle = {
+let seattle = {
 
   minHourlyCustomer: 23,
 
@@ -16,12 +16,10 @@ const seattle = {
 
   customerPerHour: 0,
 
-  avgCookiesPerCustomer: 6.3,
-
-  getCookies: function (min,max){
-    this.customerPerHour = getRandomNumberOfCustomersPerHour(min,max);
-  },
+  avgCookiesPerCustomer: 6.3
 
 
 
 };
+
+
